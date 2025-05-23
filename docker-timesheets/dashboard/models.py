5b77aa6fce5cdd4b84b743a3f47beedc80a5cd94
ungureanudaniel <ungureanu.daniel86@gmail.com>
2025-05-23 13:50:37 +0300

    Updated Dockerfile and entrypoint.prod.sh

--- docker-timesheets/dashboard/models.py
+++ docker-timesheets/dashboard/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 import datetime as dt
-
+from django.utils import timezone
 
 class ActivityProgram(models.Model):
     user = models.CharField(max_length=300)
     registration_nr = models.IntegerField()
-    registration_date = models.DateField(default=dt.date.today())
-    week = models.DateTimeField(default=dt.date.today().isocalendar()[1])
+    registration_date = models.DateField(default=timezone.now)
+    week = models.DateTimeField(default=timezone.now().isocalendar()[1])
     activity_code = models.CharField(max_length=6)
     activity_title = models.CharField(max_length=300)
 
