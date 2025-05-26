c89df64d81b23d1020d580c36c9c155e9bbc64fe
ungureanudaniel <ungureanu.daniel86@gmail.com>
2025-05-26 13:11:44 +0300

    Updated Dockerfile.prod with --worker-class

--- docker-timesheets/entrypoint.sh
+++ docker-timesheets/entrypoint.sh
@@ -9,7 +9,6 @@ function waitForServices() {
 waitForServices
 
 # Run migrations and collect static files (if necessary)
-python manage.py migrate
 python manage.py makemigrations timesheet general users report admin_dashboard auth
 python manage.py migrate
 python manage.py collectstatic --noinput
