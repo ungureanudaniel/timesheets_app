a955580a92ba2e6702fd99626620bc2c0fd17d96
ungureanudaniel <ungureanu.daniel86@gmail.com>
2025-05-28 11:27:29 +0300

    Updated entrypoint with a manual import of the .env.prod file

--- docker-timesheets/entrypoint.prod.sh
+++ docker-timesheets/entrypoint.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Source .env.dev from parent folder
-if [ -f ../.env.dev ]; then
+if [ -f ../.env.prod ]; then
   export $(grep -v '^#' ../.env.dev | xargs)
 fi
 
@@ -13,5 +13,10 @@ waitForServices
 # Ensure proper permissions
 find /app -type d -exec chmod 755 {} \;
 find /app -type f -exec chmod 644 {} \;
+
+# Run migrations and collect static files (if necessary)
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
 
 exec "$@"
